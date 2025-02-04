16db44a7f0116e12239009609a26b44f6be95dbd
Dominik Havel <dev@dmnkhvl.eu>
2025-02-04 11:34:42 +0100

    fix: error

--- src/components/ui/Error.tsx
+++ src/components/ui/Error.tsx
@@ -5,9 +5,7 @@ interface ErrorProps {
 export default function Error({ message }: ErrorProps) {
   return (
     <div className="flex text-center justify-center w-full">
-      <p className="bg-tertiary text-primary py-1 uppercase w-[200px] font-bold text-lg">
-        {message}
-      </p>
+      <p className="bg-tertiary text-primary py-1 uppercase font-bold text-lg">{message}</p>
     </div>
   )
 }

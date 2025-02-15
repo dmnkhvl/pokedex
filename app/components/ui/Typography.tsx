eb1a40309c977324a22e71b759d0c31f70381f22
Dominik Havel <dev@dmnkhvl.eu>
2025-02-15 18:01:29 +0100

    feat: new typography variants

--- app/components/ui/Typography.tsx
+++ app/components/ui/Typography.tsx
@@ -1,15 +1,23 @@
-import React from 'react'
-import { cva, type VariantProps } from 'class-variance-authority'
+import React from "react"
+import { cva, type VariantProps } from "class-variance-authority"
 
-const typographyStyles = cva('uppercase', {
+const typographyStyles = cva("uppercase", {
   variants: {
     variant: {
-      h1: 'font-semibold text-2xl',
-      h2: 'font-semibold text-lg',
+      h1: "font-semibold text-2xl",
+      h2: "font-bold text-xl",
+      body1: "uppercase text-lg",
+      body2: "uppercase",
+      body3: "uppercase text-sm",
+    },
+    onDark: {
+      false: "text-secondary",
+      true: "text-primary",
     },
   },
   defaultVariants: {
-    variant: 'h1',
+    variant: "h1",
+    onDark: true,
   },
 })
 
@@ -19,10 +27,17 @@ interface TypographyProps
   children: React.ReactNode
 }
 
-export default function Typography({ variant, children, className, ...props }: TypographyProps) {
-  const Tag = variant === 'h1' ? 'h1' : 'h2'
+export default function Typography({
+  variant,
+  onDark,
+  children,
+  className,
+  ...props
+}: TypographyProps) {
+  const Tag = variant === "h1" ? "h1" : "h2"
+
   return (
-    <Tag className={typographyStyles({ variant, className })} {...props}>
+    <Tag className={typographyStyles({ variant, onDark, className })} {...props}>
       {children}
     </Tag>
   )

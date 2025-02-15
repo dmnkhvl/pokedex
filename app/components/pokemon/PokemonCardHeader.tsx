e889955fb2d696cf9c92edf927e9d548015f7c4a
Dominik Havel <dev@dmnkhvl.eu>
2025-02-15 18:01:17 +0100

    fix: ui v2

--- app/components/pokemon/PokemonCardHeader.tsx
+++ app/components/pokemon/PokemonCardHeader.tsx
@@ -15,12 +15,12 @@ export default function PokemonCardHeader({ pokemon }: PokemonCardHeaderProps) {
       {pokemon.sprites.front_default && (
         <img src={pokemon.sprites.front_default} alt={pokemon.name} width={160} height={160} />
       )}
-      <Typography variant="h1" className="w-full bg-secondary px-4 text-center text-primary">
+      <Typography variant="h1" className="w-full bg-secondary px-4 text-center">
         {replaceHyphenWithSpace(pokemon.name)}
       </Typography>
       <div className="flex w-full justify-center rounded-b-xl border-b-card border-secondary outline-secondary">
         {pokemon.types.map((type, index) => (
-          <p key={type.type.name} className="text-lg uppercase">
+          <p key={type.type.name} className="text-lg uppercase text-secondary">
             {type.type.name}
             {index < pokemon.types.length - 1 && <span className="mx-1">&</span>}
           </p>
